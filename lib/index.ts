046acab449b31b2ea87b527c2f7e7bfd0e4ba243
alfianlensundev <alfianlensundev@gmail.com>
2023-06-10 11:36:41 +0800

    add js fucntion for get jobs

--- lib/index.ts
+++ lib/index.ts
@@ -111,58 +111,11 @@ export const print_file = async (options: PrintOptions): Promise<any> => {
  * @returns A array of all printer jobs.
  */
 export const get_jobs = async () => {
-    
-    const jsonExample = JSON.stringify([{
-        "JobStatus": 8274,
-        "Caption": null,
-        "Description": null,
-        "ElementName": null,
-        "InstanceID": null,
-        "CommunicationStatus": null,
-        "DetailedStatus": null,
-        "HealthState": null,
-        "InstallDate": null,
-        "Name": null,
-        "OperatingStatus": null,
-        "OperationalStatus": null,
-        "PrimaryStatus": null,
-        "Status": null,
-        "StatusDescriptions": null,
-        "ComputerName": "172.31.64.221",
-        "Datatype": "XPS_PASS",
-        "DocumentName": "F:\\devcode\\test.pdf",
-        "Id": 22,
-        "JobTime": 627439671,
-        "PagesPrinted": 0,
-        "Position": 1,
-        "PrinterName": "HP Ink Tank 310 series",
-        "Priority": 1,
-        "Size": 1723962,
-        "SubmittedTime": "/Date(1686361121265)/",
-        "TotalPages": 1,
-        "UserName": "SIMRS",
-        "PSComputerName": null
-    }])
-    // const listPrinter = await printers()
-    const listPrinter: Printer[] = [{
-        "id": "TWljcm9zb2Z0IFhQUyBEb2N1bWVudCBXcml0ZXI=",
-        "name": "Microsoft XPS Document Writer",
-        "driver_name": "Microsoft XPS Document Writer v4",
-        "job_count": 0,
-        "print_processor": "winprint",
-        "port_name": "PORTPROMPT:",
-        "share_name": "",
-        "computer_name": "",
-        "printer_status": 0,
-        "shared": false,
-        "type": 0,
-        "priority": 1
-    }]
+    const listPrinter = await printers()
     const allJobs: Jobs[] = []
 
     for (const printer of listPrinter){
-        // const jobs: any = await invoke('plugin:printer|get_jobs', {printername: printer.name})
-        const jobs: any = parseIfJSON(jsonExample)
+        const jobs: any = await invoke('plugin:printer|get_jobs', {printername: printer.name})
         for (const job of jobs){
             const id = encodeBase64(`${printer.name}_@_${job.Id}`);
             allJobs.push({
@@ -185,6 +138,5 @@ export const get_jobs = async () => {
         }
     }
 
-    console.log(allJobs, 'allojibs')
-    return null
+    return allJobs
 }

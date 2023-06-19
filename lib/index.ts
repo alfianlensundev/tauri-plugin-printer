e14b401a082b286d8b68499f4b6c1d9e5704776c
alfianlensun <alfianricky46@gmail.com>
2023-06-19 13:18:20 +0800

    fix dis

--- lib/index.ts
+++ lib/index.ts
@@ -65,6 +65,7 @@ export const printers = async (id: string|null = null): Promise<Printer[]> => {
     const printers: Printer[] = [];
     if (resultData.is_unix){
         const listPrinter = resultData.data.split('\n')
+        console.log(listPrinter)
         for (let i = 0; i<listPrinter.length; i++){
             const printerName: any = listPrinter[i]
             const id = encodeBase64(printerName);
